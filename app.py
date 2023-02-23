ccabf091cfb4a98368f2c11b2466dd3d1a065f40
Liam Cotter <122333311@umail.ucc.ie>
2023-02-23 17:32:48 +0000

    app.py updates

--- app.py
+++ app.py
@@ -1,48 +1,55 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, session
+from flask_session import Session
 from forms import Form
+import math as maths
+import random
 from database import get_db, close_db
 from werkzeug.security import generate_password_hash
 
+title = "Pyramid Investments Ltd."
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "1"
+app.config["SECRET_KEY"] = str(random.randbytes(20))
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_PERMANENT"] = False
 app.teardown_appcontext(close_db)
+Session(app)
 
 @app.route("/", methods=["GET","POST"])
 def home():
-    return render_template("home.html", title="Pyramid Investments Ltd.")
+    return render_template("home.html", title=title)
 
 @app.route("/buy", methods=["GET","POST"])
 def buy():
-    return render_template("buy.html", title="Pyramid Investments Ltd.")
+    return render_template("buy.html", title=title)
 
 @app.route("/sell", methods=["GET","POST"])
 def sell():
-    return render_template("sell.html", title="Pyramid Investments Ltd.")
+    return render_template("sell.html", title=title)
 
 @app.route("/query", methods=["GET","POST"])
 def query():
-    return render_template("query.html", title="Pyramid Investments Ltd.")
+    return render_template("query.html", title=title)
 
 @app.route("/login", methods=["GET","POST"])
 def login():
-    return render_template("login.html", title="Pyramid Investments Ltd.")
+    return render_template("login.html", title=title)
 
 @app.route("/register", methods=["GET","POST"])
 def register():
-    return render_template("register.html", title="Pyramid Investments Ltd.")
+    return render_template("register.html", title=title)
 
 @app.route("/account", methods=["GET","POST"])
 def account():
-    return render_template("account.html", title="Pyramid Investments Ltd.")
+    return render_template("account.html", title=title)
 
 @app.route("/about", methods=["GET","POST"])
 def about():
-    return render_template("about.html", title="Pyramid Investments Ltd.")
+    return render_template("about.html", title=title)
 
 @app.route("/admin", methods=["GET","POST"])
 def admin():
-    return render_template("admin.html", title="Pyramid Investments Ltd.")
+    return render_template("admin.html", title=title)
 
 @app.route("/gamble", methods=["GET","POST"])
 def gamble():
-    return render_template("gamble.html", title="Pyramid Investments Ltd.")
+    return render_template("gamble.html", title=title)
